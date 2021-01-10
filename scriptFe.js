49c7330ce3c51ff659bcef4cede3e77536c7918b
sushil <65329897+sushilthelearner@users.noreply.github.com>
2021-01-10 15:00:52 +0530

    Update scriptFe.js

--- scriptFe.js
+++ scriptFe.js
@@ -126,7 +126,8 @@ const onFileUploadSuccess = (res) => {
   emailForm[2].innerText = "Send";
   progressContainer.style.display = "none"; // hide the box
 
-  const { file: url } = JSON.parse(res);
+  const { file: url } = res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}` });
+  
   console.log(url);
   sharingContainer.style.display = "block";
   fileURL.value = url;
