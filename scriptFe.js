6de70d553e9d7771d105ab5728196501adc06398
sushil <65329897+sushilthelearner@users.noreply.github.com>
2021-01-10 23:15:57 +0530

    Update scriptFe.js

--- scriptFe.js
+++ scriptFe.js
@@ -126,14 +126,11 @@ const onFileUploadSuccess = (res) => {
   emailForm[2].innerText = "Send";
   progressContainer.style.display = "none"; // hide the box
 
- const show=({file :url}) =>{
-   console.log(url)
-   fileURL.value=url
- };
+ 
   const { file: url } = JSON.parse(res);
   console.log(url);
   sharingContainer.style.display = "block";
-//   fileURL.value = url;
+   fileURL.value = url;
 };
 
 emailForm.addEventListener("submit", (e) => {
